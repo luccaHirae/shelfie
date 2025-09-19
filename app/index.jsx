0e01c27a4dc65a25fd69c633e1918e4b75694369
Bruno Hirae <brunodeluca117@outlook.com>
2025-09-19 12:06:51 -0300

    feat: setup Expo router

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium error, molestiae quibusdam, facilis doloremque animi enim odit aspernatur distinctio ea perspiciatis iusto, corrupti quis sunt tempore explicabo. Qui, cupiditate hic.</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
