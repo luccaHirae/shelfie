2b2bc13f17681545e2190074d68878137eaebb6d
Bruno Hirae <brunodeluca117@outlook.com>
2025-09-22 10:36:23 -0300

    feat: update Home component with logo and styling

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/img/logo_light.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium error, molestiae quibusdam, facilis doloremque animi enim odit aspernatur distinctio ea perspiciatis iusto, corrupti quis sunt tempore explicabo. Qui, cupiditate hic.</Text>
+    <View style={styles.container}>
+      <Image
+        source={Logo}
+        style={styles.img}
+      />
+
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  img: {
+    marginVertical: 20,
+  },
+})
