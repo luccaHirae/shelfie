54467baa391af5cd6419e69f47148beb993bcbeb
Bruno Hirae <brunodeluca117@outlook.com>
2025-09-22 11:32:39 -0300

    feat: implement dark mode support and add color constants

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,36 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import { Stack } from 'expo-router'
+import { Colors } from '../constants/colors'
+import { StatusBar } from 'expo-status-bar'
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#ddd',
-          },
-          headerTintColor: '#333'
-        }}
-      >
-        <Stack.Screen
-          name='index'
-          options={{
-            title: 'Home',
+      <>
+        <StatusBar value='auto' />
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.navBackground,
+            },
+            headerTintColor: theme.title,
           }}
-        />
-        <Stack.Screen
-          name='about'
-          options={{
-            title: 'About',
-          }}
-        />
-        <Stack.Screen
-          name='contact'
-          options={{
-            title: 'Contact',
-            headerShown: false,
-          }}
-        />
-      </Stack>
+        >
+          <Stack.Screen
+            name='index'
+            options={{
+              title: 'Home',
+            }}
+          />
+          <Stack.Screen
+            name='about'
+            options={{
+              title: 'About',
+            }}
+          />
+          <Stack.Screen
+            name='contact'
+            options={{
+              title: 'Contact',
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </>
   )
 }
 
