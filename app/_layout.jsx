2dff9a915492a71e70073f7474c0e1e3080c3053
Bruno Hirae <brunodeluca117@outlook.com>
2025-09-23 12:33:51 -0300

    feat: update RootLayout to streamline navigation; remove About and Contact components

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -18,25 +18,8 @@ const RootLayout = () => {
             headerTintColor: theme.title,
           }}
         >
-          <Stack.Screen
-            name='index'
-            options={{
-              title: 'Home',
-            }}
-          />
-          <Stack.Screen
-            name='about'
-            options={{
-              title: 'About',
-            }}
-          />
-          <Stack.Screen
-            name='contact'
-            options={{
-              title: 'Contact',
-              headerShown: false,
-            }}
-          />
+          <Stack.Screen name='index' options={{ title: 'Home' }} />
+          <Stack.Screen name='(auth)' options={{ headerShown: false }} />
         </Stack>
       </>
   )
