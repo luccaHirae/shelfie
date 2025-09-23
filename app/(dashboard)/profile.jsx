52f7072613c9935588f1ce2c6541c31898aff92c
Bruno Hirae <brunodeluca117@outlook.com>
2025-09-23 14:53:09 -0300

    feat: add dashboard layout and screens for profile, books, and create functionalities

---
+++ app/(dashboard)/profile.jsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native'
+import ThemedView from '../../components/ThemedView'
+import ThemedText from '../../components/ThemedText'
+import Spacer from '../../components/Spacer'
+
+const Profile = () => {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText title style={styles.heading}>Your email</ThemedText>
+      <Spacer />
+
+      <ThemedText>Time to start reading some books...</ThemedText>
+      <Spacer />
+    </ThemedView>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    textAlign: 'center'
+  }
+})
