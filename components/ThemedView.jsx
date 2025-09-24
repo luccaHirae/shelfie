b644a3efd09a4003aec40e5d21f318685ddb34cf
Bruno Hirae <brunodeluca117@outlook.com>
2025-09-24 11:05:51 -0300

    feat: enhance ThemedView component to support safe area insets

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,12 +1,30 @@
 import { useColorScheme, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { Colors } from '../constants/colors'
 
-const ThemedView = ({ style, ...rest }) => {
+const ThemedView = ({ style, safe = false, ...rest }) => {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
 
+  const insets = useSafeAreaInsets()
+
+  if (safe) {
+    return (
+      <View 
+        style={[{ 
+          backgroundColor: theme.background, 
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        }, 
+          style 
+        ]}
+        {...rest}
+      />
+    )
+  }
+  
   return (
-    <View 
+    <View
       style={[{ backgroundColor: theme.background }, style ]}
       {...rest}
     />
