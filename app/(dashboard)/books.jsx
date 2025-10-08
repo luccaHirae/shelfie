c966098d591a630904e056f3c40233c42ea342b0
Bruno Hirae <brunodeluca117@outlook.com>
2025-10-08 15:55:34 -0300

    feat: implement FlatList for displaying books with ThemedCard and improved layout

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet } from 'react-native'
+import { FlatList, Pressable, StyleSheet } from 'react-native'
+import { useBooks } from '../../hooks/useBooks'
+import { Colors } from '../../constants/colors'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedCard from '../../components/ThemedCard'
 
 const Books = () => {
+  const { books } = useBooks();
+
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView style={styles.container} safe>
+      <Spacer />
+
       <ThemedText title style={styles.heading}>Your reading list</ThemedText>
 
       <Spacer />
+
+      <FlatList 
+        data={books}
+        keyExtractor={(item) => item.$id}
+        contentContainerStyle={styles.list}
+        renderItem={({ item }) => (
+          <Pressable>
+            <ThemedCard styles={styles.card}>
+              <ThemedText styles={styles.title}>{item.title}</ThemedText>
+              <ThemedText>Written by {item.author}</ThemedText>
+            </ThemedCard>
+          </Pressable>
+        )}
+      />
     </ThemedView>
   )
 }
@@ -25,5 +46,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     textAlign: 'center'
+  },
+  list: {
+    marginTop: 40,
+  },
+  card: {
+    width: '90%',
+    marginHorizontal: '5%',
+    marginVertical: 10,
+    padding: 10,
+    paddingLeft: 14,
+    borderLeftColor: Colors.primary,
+    borderLeftWidth: 4,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   }
 })
