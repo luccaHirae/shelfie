d96297fcd0b1883417328fc63e10fc38a40090c6
Bruno Hirae <brunodeluca117@outlook.com>
2025-10-08 15:45:38 -0300

    feat: enhance BooksProvider to fetch books based on user ID and manage state with useEffect

--- contexts/Books.jsx
+++ contexts/Books.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { ID, Permission, Role } from "react-native-appwrite";
+import { createContext, useEffect, useState } from "react";
+import { ID, Permission, Query, Role } from "react-native-appwrite";
 import { COLLECTION_ID, DATABASE_ID, databases } from "../lib/appwrite";
 import { useUser } from "../hooks/useUser";
 
@@ -12,7 +12,11 @@ export const BooksProvider = ({ children }) => {
 
   async function fetchBooks() {
     try {
-      
+      const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [
+        Query.equal('userId', user.$id)
+      ])
+
+      setBooks(response.documents);
     } catch (e) {
       throw Error(e.message || e);
     }
@@ -51,6 +55,14 @@ export const BooksProvider = ({ children }) => {
       throw Error(e.message || e);
     }
   }
+
+  useEffect(() => {
+    if (user?.$id) {
+      fetchBooks();
+    } else {
+      setBooks([]);
+    }
+  }, [user]);
 
   const value = { books, setBooks, fetchBooks, fetchBookById, createBook, deleteBook };
 
